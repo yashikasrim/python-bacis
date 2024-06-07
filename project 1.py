d3d5234cf68898ee2fe0ffbe38e62a20923e6af7
yashikasrim <selviutc@gmail.com>
2024-06-07 17:13:16 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,11 @@
+colour=input("enter the colour")
+if((colour=="red")):
+    print("YOU SHOULD STOP")
+elif((colour=="yellow")):
+    print("BE READY")
+elif((colour=="green")):
+    print("YOU CAN GO")
+else:
+    print("enter the valid colour")
+
+
